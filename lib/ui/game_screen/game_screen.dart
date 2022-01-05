21a8758d9b8d278874c91bf6e7aac10a1a7f3b14
Vorkytaka <vorkytaka@gmail.com>
2022-01-05 22:41:32 +0300

    - Base for game screen

--- lib/ui/game_screen/game_screen.dart
+++ lib/ui/game_screen/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:arithmetic/widget/you_button.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatelessWidget {
@@ -7,10 +8,108 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(
-        child: Text('game'),
-      )),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            Expanded(
+              flex: 1,
+              child: _Header(),
+            ),
+            Expanded(
+              flex: 2,
+              child: _Keyboard(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '99 + 100',
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          Text(
+            '199',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Keyboard extends StatelessWidget {
+  const _Keyboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            YouButton(child: Text('7')),
+            YouButton(child: Text('8')),
+            YouButton(child: Text('9')),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            YouButton(child: Text('4')),
+            YouButton(child: Text('5')),
+            YouButton(child: Text('6')),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            YouButton(child: Text('1')),
+            YouButton(child: Text('2')),
+            YouButton(child: Text('3')),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            YouButton(child: Text('<')),
+            YouButton(child: Text('0')),
+            YouButton(child: Text('=')),
+          ],
+        ),
+      ],
     );
   }
 }
