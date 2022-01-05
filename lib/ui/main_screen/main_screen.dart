db9918ec25dbf2d116fbc5ac91733510b4524c47
Vorkytaka <vorkytaka@gmail.com>
2022-01-05 20:32:22 +0300

    - Move `YouButton`

--- lib/ui/main_screen/main_screen.dart
+++ lib/ui/main_screen/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:arithmetic/widget/you_button.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -10,87 +11,10 @@ class MainScreen extends StatelessWidget {
     return const Scaffold(
       body: Center(
         child: YouButton(
-          text: '+',
+          child: Text('='),
           width: 100,
           height: 100,
         ),
-      ),
-    );
-  }
-}
-
-class YouButton extends StatefulWidget {
-  final String text;
-  final double width;
-  final double height;
-  final double startRadius;
-
-  const YouButton({
-    Key? key,
-    required this.text,
-    required this.width,
-    required this.height,
-  })  : startRadius = width > height ? height : width,
-        super(key: key);
-
-  @override
-  State<YouButton> createState() => _YouButtonState();
-}
-
-class _YouButtonState extends State<YouButton> with SingleTickerProviderStateMixin {
-  late final AnimationController animation;
-  Animation<double>? radius;
-
-  @override
-  void initState() {
-    super.initState();
-    animation = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    radius ??= Tween<double>(begin: widget.startRadius, end: widget.startRadius / 5).animate(animation);
-  }
-
-  @override
-  void dispose() {
-    animation.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) {
-        return Material(
-          color: Theme.of(context).primaryColor,
-          borderRadius: BorderRadius.all(Radius.circular(radius?.value ?? 50)),
-          clipBehavior: Clip.hardEdge,
-          child: InkWell(
-            onTapDown: (d) {
-              animation.forward();
-            },
-            onTap: () {
-              animation.reverse();
-            },
-            onTapCancel: () {
-              animation.reverse();
-            },
-            child: Container(
-              width: widget.width,
-              height: widget.height,
-              alignment: Alignment.center,
-              child: child,
-            ),
-          ),
-        );
-      },
-      child: Text(
-        widget.text,
-        textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.headline4?.copyWith(color: Theme.of(context).colorScheme.onPrimary),
       ),
     );
   }
---
+++ lib/widget/you_button.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class YouButton extends StatefulWidget {
+  final Widget child;
+  final double width;
+  final double height;
+  final double startRadius;
+  final Duration duration;
+  final Duration reverseDuration;
+  final TextStyle? textStyle;
+
+  const YouButton({
+    Key? key,
+    required this.child,
+    required this.width,
+    required this.height,
+    this.duration = const Duration(milliseconds: 150),
+    this.reverseDuration = const Duration(milliseconds: 100),
+    this.textStyle,
+  })  : startRadius = width > height ? height : width,
+        super(key: key);
+
+  @override
+  State<YouButton> createState() => _YouButtonState();
+}
+
+class _YouButtonState extends State<YouButton> with SingleTickerProviderStateMixin {
+  late final AnimationController animation;
+  Animation<double>? radius;
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+      reverseDuration: widget.reverseDuration,
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    radius ??= Tween<double>(begin: widget.startRadius, end: widget.startRadius / 5).animate(animation);
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Material(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.all(Radius.circular(radius?.value ?? 50)),
+          clipBehavior: Clip.hardEdge,
+          child: InkWell(
+            onTapDown: (_) => animation.forward(),
+            onTap: () => animation.reverse(),
+            onTapCancel: () => animation.reverse(),
+            child: Container(
+              width: widget.width,
+              height: widget.height,
+              alignment: Alignment.center,
+              child: child,
+            ),
+          ),
+        );
+      },
+      child: DefaultTextStyle(
+        style: widget.textStyle ??
+            Theme.of(context).textTheme.headline4!.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+        child: widget.child,
+      ),
+    );
+  }
+}
